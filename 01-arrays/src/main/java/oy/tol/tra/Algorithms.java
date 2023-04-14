16aef7f6a4c424e18e4742ea9f28054a96e8a3fb
Jack Sundholm <sundholm38@gmail.com>
2023-04-14 11:51:26 +0000

    Update 2 files
    - /01-arrays/src/main/java/oy/tol/tra/Algorithms.java
    - /02-mode/src/main/java/oy/tol/tra/Algorithms.java

--- 01-arrays/src/main/java/oy/tol/tra/Algorithms.java
+++ 01-arrays/src/main/java/oy/tol/tra/Algorithms.java
@@ -5,39 +5,27 @@ public class Algorithms<T extends Comparable<T>>{
 
     public static <T extends Comparable<T>> void sort(T [] array) {
 
-        try{
+        for(int i = 0; i < array.length; i++){
+            for(int k = 1; k < (array.length-i); k++){             
+                int j = k - 1;
 
-            for(int i = 0; i < array.length; i++){
-                for(int k = 1; k < (array.length-i); k++){             
-                    int j = k - 1;
-
-                    if(array[j].compareTo(array[k]) > 0){
-                        swap(array, k, j);
-                    }
+                if(array[j].compareTo(array[k]) > 0){
+                    swap(array, k, j);
                 }
             }
-            
-        }catch(NullPointerException e){
-            System.out.println("NullPointerExeption Caught!");
-        }
+        }      
     }
 
 
     public static <T> void reverse(T [] array) {
 
-        try{
+        int i = 0;
 
-            int i = 0;
-
-            while (i < array.length/2) {
-                int k = array.length-i-1;
-                    swap(array, i, k); 
-                i++;
-            }   
-
-        }catch(NullPointerException e){
-            System.out.println("NullPointerExeption Caught!");
-        }
+        while (i < array.length/2) {
+            int k = array.length-i-1;
+                swap(array, i, k); 
+            i++;
+        }   
     }
 
     public static <T> void swap(T [] array, int first, int second){
